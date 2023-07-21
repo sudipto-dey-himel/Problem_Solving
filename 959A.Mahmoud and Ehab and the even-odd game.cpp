9748277355af84df21871d52433f95d0cf5e74e0
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-07-21 16:00:39 +0600

    Create 959A.Mahmoud and Ehab and the even-odd game.cpp

---
+++ 959A.Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define tc ll t;cin>>t;while(t--)
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+    opt();
+    
+    ll n;
+    cin>>n;
+    if(n%2==0)
+    cout<<"Mahmoud"<<endl;
+    else
+    cout<<"Ehab"<<endl;
+}
