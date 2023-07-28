64ea8daedc8a1fb13a68f15a9668dadbfe188ae6
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-07-28 18:32:53 +0600

    Update 282A.Bit++.cpp

--- 282A.Bit++.cpp
+++ 282A.Bit++.cpp
@@ -1,17 +1,25 @@
-
-#include<iostream>
+#include<bits/stdc++.h>
 #include<string>
+#define ll long long
+#define endl '\n'
+#define tc() ll t;cin>>t;while(t--)
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
 using namespace std;
+
 int main() {
-    int n, c=0;
-    cin>>n;
-    for(int i=0; i<n; i++) {
-        string s;
-        cin>>s;
-        if(s[1]=='+')
-        c+=1;
-        else
-        c-=1;
+    opt();
+    
+    ll ans=0;
+    tc() {
+    string s;
+    cin>>s;
+    if(s=="X++" || s=="++X")
+    ans+=1;
+    else
+    ans-=1;
     }
-    cout<<c<<endl;
+    cout<<ans<<endl;
+
+    return 0;
 }
