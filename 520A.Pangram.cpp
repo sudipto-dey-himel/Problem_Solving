97001c9c7fff340564561b50a97199774408ec27
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-08-04 11:15:07 +0600

    Update 520A.Pangram.cpp

--- 520A.Pangram.cpp
+++ 520A.Pangram.cpp
@@ -30,3 +30,43 @@ int main() {
     cout<<"NO"<<endl;
     return 0;
 }
+
+/* Another valid solution ::
+#include<bits/stdc++.h>
+#include<string>
+#define ll long long
+#define endl '\n'
+#define starr(a) sort(a, a+n)
+#define ststr(a) sort(a.begin(), a.end())
+#define lc(a) transform(a.begin(), a.end(), a.begin(), ::tolower)
+#define vecst(a) sort(a.begin(), a.end())
+#define tc() ll t;cin>>t;while(t--)
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+
+void solve() {
+    ll n, count=0;
+    cin>>n;
+    string s;
+    cin>>s;
+    lc(s);
+    ststr(s);
+    for(ll i=0; i<n; i++) {
+        if(s[i]!=s[i+1])
+        count++;
+    }
+    if(count==26)
+    cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+}
+int main() {
+
+    opt();
+
+    solve();
+
+    return 0;
+}
+*/
