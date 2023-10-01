01d0bf1d38d4dd46f06b30b5c944119fd38ce12f
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2023-10-01 21:02:53 +0600

    Create 1809B.Points on Plane.cpp

---
+++ 1809B.Points on Plane.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl '\n'
+#define sq(a) sqrt(a)
+#define tc() ll testCase;cin>>testCase;while(testCase--)
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+using namespace std;
+
+void solve() {
+    ll n;
+    cin>>n;
+    cout<<(ll)sq(n-1)<<endl;
+}
+
+int main() {
+
+    opt();
+
+    tc() {
+        solve();
+    }
+
+    return 0;
+}
