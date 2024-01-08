325f37e1a8f6c50171d705324efddb9fc9a487ce
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-01-08 13:00:18 +0600

    Create 755A.PolandBall and Hypothesis.cpp

---
+++ 755A.PolandBall and Hypothesis.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define sortV(V) sort(allV(V))
+#define revV(V) reverse(allV(V))
+#define sortA(arr, size) sort(allA(arr, size))
+#define revA(arr, size) reverse(allA(arr, size))
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define tc() ll t;cin>>t;while(t--)
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+using namespace std;
+
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+    if (n == 2)
+        return true;
+    if (n % 2 == 0)
+        return false;
+    for (ll i = 3; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+void solve() {
+    ll n, ans = 0;
+    cin>>n;
+    for(ll i=1; ; i++) {
+        ll temp = (n*i)+1;
+        if(!isPrime(temp)) {
+            ans = i;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    opt();
+    solve();
+    return 0;
+}
