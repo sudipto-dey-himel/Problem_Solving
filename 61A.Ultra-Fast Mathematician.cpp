e0cc24d6f7f4dad0cf16b54b9ab7a859eed8b6e9
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-02-04 12:17:47 +0600

    Update 61A.Ultra-Fast Mathematician.cpp

--- 61A.Ultra-Fast Mathematician.cpp
+++ 61A.Ultra-Fast Mathematician.cpp
@@ -6,7 +6,7 @@ int main() {
     string s1, s2;
     cin>>s1>>s2;
     int j=0;
-    for(int i=9; i<s1.size(); i++) {
+    for(int i=0; i<s1.size(); i++) {
         if(s1[i]==s2[i])
         s1[j++]='0';
         else
