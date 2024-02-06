42ecf35bba3749f7a52640381693135501eb30e9
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-02-06 10:20:01 +0600

    Update 1475A.Odd Divisor.cpp

--- 1475A.Odd Divisor.cpp
+++ 1475A.Odd Divisor.cpp
@@ -1,48 +1,50 @@
-#include<bits/stdc++.h>
-#include<vector>
-#define ll     long long
-#define endl   '\n'
-#define sq(a)   sqrt(a)
-#define vecst(a) sort(a.begin(), a.end())
-#define tc()   ll test_case;cin>>test_case;while(test_case--)
-#define opt()  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define HA cout<<"YES"<<endl
+#define Ha cout<<"Yes"<<endl
+#define ha cout<<"yes"<<endl
+#define NA cout<<"NO"<<endl
+#define Na cout<<"No"<<endl
+#define na cout<<"no"<<endl
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define sortV(V) sort(allV(V))
+#define revV(V) reverse(allV(V))
+#define sortA(arr, size) sort(allA(arr, size))
+#define revA(arr, size) reverse(allA(arr, size))
+#define sumV(a) accumulate(a.begin(), a.end(), 0)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define tc() ll t;cin>>t;while(t--)
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
 
 using namespace std;
 
 void solve() {
     ll n;
     cin>>n;
-    
-    /*vector<ll>ans;
-    for(ll i=1; i<=sq(n); i++) {
-        if(n%i == 0) {
-            ans.push_back(i);
-            if(i != n/i)
-            ans.push_back(n/i);
-        }
+    while(!(n&1)) {
+        n >>= 1;
     }
-    bool check=false;
-    for(ll i=1; i<ans.size(); i++) {
-        if(ans[i]&1)
-        check=true;
-    }
-    if(check) cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;*/
-    
-    while(n%2==0) {
-        n/=2;
-    }
-    if(n>1) cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
+    (n > 1) ? HA : NA;
 }
 
 int main() {
-
     opt();
-
     tc() {
         solve();
     }
-
     return 0;
 }
