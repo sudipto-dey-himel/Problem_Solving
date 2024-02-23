9a84540097bf5fdb2309225ceefb6504339e9e15
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-02-23 14:22:40 +0600

    Update 579A.Raising Bacteria.cpp

--- 579A.Raising Bacteria.cpp
+++ 579A.Raising Bacteria.cpp
@@ -1,22 +1,84 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #define ll long long
+#define ld long double
 #define endl '\n'
+#define bug(a) cout << #a << " : " << a << endl;
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define sortA(arr, size) sort(allA(arr, size))
+#define revA(arr, size) reverse(allA(arr, size))
+#define sortV(V) sort(allV(V))
+#define revV(V) reverse(allV(V))
+#define revS(s) reverse(s.begin(), s.end())
+#define sumV(a) accumulate(a.begin(), a.end(), 0)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
 #define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
 #define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
 
 using namespace std;
 
+// *** approach -> 1
+
 void solve() {
-    ll x;
-    cin>>x;
-    ll count=0;
-    while(x!=0) {
-        ll r = x%2;
-        count+=r;
-        x/=2;
-    }
-    cout<<count<<endl;
+    ll x; cin>>x;
+
+    cout<<__builtin_popcount(x)<<endl;
 }
+
+// *** approaach -> 2
+
+// vector<ll> decToBin(ll n) {  
+//     vector<ll> bin; 
+//     while(n != 0) {
+//         bin.push_back(n&1);
+//         n >>= 1;
+//     }
+//     return bin;
+// }
+
+// void solve() {
+//     ll x; cin>>x;
+//     vector<ll> temp = decToBin(x);
+//     revV(temp);
+//     ll count = 0;
+//     for(auto u : temp) {
+//         if(u & 1) count++;
+//     }
+//     cout<<count<<endl;
+// }
+
+
+// *** approach -> 3
+
+// void solve() {
+//     ll x; cin>>x;
+
+//     ll count=0;
+//     while(x!=0) {
+//         ll r = x%2;
+//         count+=r;
+//         x/=2;
+//     }
+//     cout<<count<<endl;
+// }
+
 
 int main() {
     opt();
