4a670ce6b8d34aa01ba676e2947ed3fdd25e4293
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-02-28 10:10:45 +0600

    Create 1933B. Turtle Math: Fast Three Task.cpp

---
+++ 1933B. Turtle Math: Fast Three Task.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define bug(a) cout << #a << " : " << a << endl;
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define sortA(arr, size) sort(allA(arr, size))
+#define revA(arr, size) reverse(allA(arr, size))
+#define sortV(V) sort(allV(V))
+#define revV(V) reverse(allV(V))
+#define revS(s) reverse(s.begin(), s.end())
+#define sumV(a) accumulate(a.begin(), a.end(), 0)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
+
+using namespace std;
+
+void solve() {
+    ll n; cin>>n;
+    vector<ll> V(n);
+    ll sum = 0;
+    for(auto &x : V) {
+        cin>>x;
+        sum += x;
+    }
+    if(sum%3 == 0) {
+        cout<<0<<endl;
+        return;
+    }
+    sortV(V);
+    ll count = 0, i=0;
+    if(n == 1) {
+        cout<<1<<endl;
+        return;
+    }
+    
+    for(ll i=0; i<n; i++) {
+        ll t = sum - V[i];
+        ll t2 = sum+1;
+        if(t % 3 == 0) {
+            cout<<1<<endl;
+            return;
+        }
+        else if(t2 % 3 == 0) {
+            cout<<1<<endl;
+            return;
+        }
+    }
+    cout<<2<<endl;
+}
+
+int main() {
+    opt();
+    tc() {
+        solve();
+    }
+    return 0;
+}
