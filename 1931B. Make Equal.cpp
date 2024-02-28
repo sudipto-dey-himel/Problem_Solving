8e1a57b7b67c7b9a2065de1081d21ab89b9f7966
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-02-28 09:39:40 +0600

    Create 1931B. Make Equal.cpp

---
+++ 1931B. Make Equal.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define HA cout<<"YES"<<endl
+#define Ha cout<<"Yes"<<endl
+#define ha cout<<"yes"<<endl
+#define NA cout<<"NO"<<endl
+#define Na cout<<"No"<<endl
+#define na cout<<"no"<<endl
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define sortV(V) sort(allV(V))
+#define revV(V) reverse(allV(V))
+#define sortA(arr, size) sort(allA(arr, size))
+#define revA(arr, size) reverse(allA(arr, size))
+#define sumV(a) accumulate(a.begin(), a.end(), 0)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define tc() ll t;cin>>t;while(t--)
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+using namespace std;
+
+void solve() {
+    ll n;
+    cin>>n;
+    vector<ll> V(n);
+    for(auto &x : V) cin>>x;
+    ll t = sumV(V)/n;
+
+    for(ll i=0; i<n-1; i++) {
+        if(V[i] >= t) V[i+1] += V[i]-t;
+        else {
+            NA; return;
+        }
+    }
+    HA;
+}
+
+int main() {
+    opt();
+    tc() {
+        solve();
+    }
+    return 0;
+}
