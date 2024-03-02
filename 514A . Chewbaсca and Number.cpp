6dd9b91cc06d2776bc5ee7b1ee4812d269d27cb8
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-03-02 14:31:06 +0600

    Update 514A . Chewbaсca and Number.cpp

--- 514A . Chewbaсca and Number.cpp
+++ 514A . Chewbaсca and Number.cpp
@@ -2,35 +2,35 @@
 #define ll long long
 #define ld long double
 #define endl '\n'
-#define HA cout<<"YES"<<endl
-#define Ha cout<<"Yes"<<endl
-#define ha cout<<"yes"<<endl
-#define NA cout<<"NO"<<endl
-#define Na cout<<"No"<<endl
-#define na cout<<"no"<<endl
+#define bug(a) cout << #a << " : " << a << endl;
 #define allV(V) (V).begin(), (V).end()
 #define allA(arr, size) arr, arr+size
-#define sortV(V) sort(allV(V))
-#define revV(V) reverse(allV(V))
-#define sortA(arr, size) sort(allA(arr, size))
-#define revA(arr, size) reverse(allA(arr, size))
+#define allS(s) s.begin(), s.end()
 #define sumV(a) accumulate(a.begin(), a.end(), 0)
 #define fi first
 #define se second 
 #define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
 #define maxV(V) *max_element(V.begin(), V.end())
 #define mnV(V) *min_element(V.begin(), V.end())
 #define mx(a, b, c) max(a, max(b, c))
 #define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
 #define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
 #define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 
-const int MAX_N = 1e5 + 5;
+const int MAX_SIZE = 1e5 + 5;
 const ll MOD = 1e9 + 7;
-const ll INF = 1e9;
-const ld EPS = 1e-9;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
 
 using namespace std;
+
+// *** approach -> 2
 
 void solve() {
     ll n;
@@ -47,12 +47,33 @@ void solve() {
         cout<<t<<endl; return;
     }
     revV(V);
-    if((9-V[0]) == 0) cout<<V[0];
-    else cout<<min(V[0], 9-V[0]);
-        for(ll i=1; i<x; i++) {
-            cout<<min(V[i], (9-V[i]));
-        }
+
+    if ((9 - V[0]) == 0)
+        cout << V[0];
+    else
+        cout << min(V[0], 9 - V[0]);
+    for (ll i = 1; i < x; i++) {
+        cout << min(V[i], (9 - V[i]));
+    }
 }
+
+
+// *** approach -> 2
+
+// void solve() {
+//     string s;
+//     cin>>s;
+//     char temp = s[0];
+//     string ans;
+//     for(ll i=0; i<s.size(); i++) {
+//         ll t  = s[i]-'0';
+//         ll x = 9-t;
+//         ans += (min(t, x))+'0';
+//     }
+//     if(ans[0] == '0') ans[0] = temp;
+
+//     cout<<ans<<endl;
+// }
 
 int main() {
     opt();
