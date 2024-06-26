e18ea36d516a65a725bae2307dc41e6b2ed751bf
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-06-26 13:39:00 +0600

    Create 1355A. Sequence with Digits.cpp

---
+++ 1355A. Sequence with Digits.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define bug(a) cout << #a << " : " << a << endl;
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define sortA(arr, size) sort(allA(arr, size))
+#define revA(arr, size) reverse(allA(arr, size))
+#define sortV(V) sort(allV(V))
+#define revV(V) reverse(allV(V))
+#define revS(s) reverse(s.begin(), s.end())
+#define sumV(a) accumulate(a.begin(), a.end(), 0)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
+#define mxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
+
+using namespace std;
+
+ll gun(ll n) {
+    vector<ll> Gun;
+    while(n != 0) {
+        Gun.pb(n%10);
+        n /= 10;
+    }
+    return mxV(Gun) * mnV(Gun);
+}
+
+void solve() {
+    ll n, k;  cin>>n>>k;
+    for(ll i=1; i<k; i++) {
+        ll temp = gun(n);
+        if(temp == 0) break;
+        n += temp;
+    }
+    cout<<n<<endl;
+}
+
+int main() {
+    opt();
+    tc() {
+        solve();
+    }
+    return 0;
+}
