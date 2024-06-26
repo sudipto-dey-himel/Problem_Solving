01d385a6aa1be20f09dfa2be1594ed7f9a284f89
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-06-26 14:21:48 +0600

    Update 96A.Football.cpp

--- 96A.Football.cpp
+++ 96A.Football.cpp
@@ -1,36 +1,57 @@
-
 #include <bits/stdc++.h>
-#include <string>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define bug(a) cout << #a << " : " << a << endl;
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define allS(s) s.begin(), s.end()
+#define sumV(a) accumulate(a.begin(), a.end(), 0LL)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
+
 using namespace std;
 
-/**
- *  @author Sudipto Dey Himel
- */
-
-int main()
-{
-    string s;
-    cin >> s;
-
-    int count = 1;
-    char temp = s[0];
-
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i+1] == temp)
-            count++;
-        else
-        {
-            count = 1;
-            temp = s[i+1];
-        }
-        if (count >= 7)
-        {
-            cout << "YES" << endl;
-            return 0;
+void solve() {
+    string s; cin>>s;
+    ll count = 0, ans = 0, count2 = 0, ans2 = 0;
+    for(auto i : s) {
+        if(i == '0') count++;
+        else {
+            ans = max(ans, count); count = 0;
         }
     }
-    cout << "NO" << endl;
+    for(auto i : s) {
+        if(i == '1') count2++;
+        else {
+            ans2 = max(ans2, count2); count2 = 0;
+        }
+    }
+    (max(ans, count) >= 7 || max(ans2, count2) >= 7) ? HA : NA;
+}
 
+int main() {
+    opt();
+    //tc() {
+        solve();
+    //}
     return 0;
 }
