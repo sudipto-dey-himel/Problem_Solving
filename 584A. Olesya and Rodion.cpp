f045f1219c903b7d9b6eadb20fefbccfc7bb2293
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-06-26 14:24:49 +0600

    Create 584A. Olesya and Rodion.cpp

---
+++ 584A. Olesya and Rodion.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define bug(a) cout << #a << " : " << a << endl;
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define allS(s) s.begin(), s.end()
+#define sumV(a) accumulate(a.begin(), a.end(), 0LL)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
+
+using namespace std;
+
+void solve() {
+    ll n, t; cin>>n>>t;
+    if(t >= 10 && n == 1) {
+        cout<<-1; 
+    }
+    else if(t >= 10 && n > 1){
+        if(n == 2) cout<<t;
+        else {
+            cout<<t;
+            for(ll i=1; i<=n-2; i++) cout<<0;
+        }
+    }
+    else {
+        for(ll i=1; i<=n; i++) cout<<t;
+    }
+    
+}
+
+int main() {
+    opt();
+    //tc() {
+        solve();
+    //}
+    return 0;
+}
