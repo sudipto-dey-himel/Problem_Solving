6fb88ea0491d67d6d2bfa14b10d9ad746de49f01
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-06-26 13:39:42 +0600

    Create 1934A. Too Min Too Max.cpp

---
+++ 1934A. Too Min Too Max.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define endl '\n'
+#define bug(a) cout << #a << " : " << a << endl;
+#define allV(V) (V).begin(), (V).end()
+#define allA(arr, size) arr, arr+size
+#define allS(s) s.begin(), s.end()
+#define sumV(a) accumulate(a.begin(), a.end(), 0)
+#define fi first
+#define se second 
+#define pb(V) push_back(V)
+#define ppb(V) pop_back(V)
+#define maxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define tc() ll t;cin>>t;while(t--)
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = INT_MAX;
+const ld EPS = INT_MIN;
+
+using namespace std;
+
+void solve() {
+    ll n; cin>>n;
+    vector<ll> V(n);
+    for(auto &x : V) cin>>x;
+    sort(allV(V));
+    ll i=0, j=n-1, k=1, l=n-2;
+    cout<<(abs(V[i]-V[j])+abs(V[j]-V[k])+abs(V[k]-V[l])+abs(V[l]-V[i]))<<endl;
+}
+
+int main() {
+    opt();
+    tc() {
+        solve();
+    }
+    return 0;
+}
