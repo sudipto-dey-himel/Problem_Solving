5a0c11404bd2507dfa35ab322f6ebbfd40e76cd1
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-06-29 20:18:24 +0600

    Create 1980A. Problem Generator.cpp

---
+++ 1980A. Problem Generator.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef double ld;
+
+#define endl '\n'
+#define fi first
+#define se second 
+#define pb push_back
+#define ppb pop_back
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define mem(a, b) memset(a, b, sizeof(a))
+#define tc() ll t;cin>>t;while(t--)
+#define all(V) (V).begin(), (V).end()
+#define bug(a) cout << #a << " : " << a << endl;
+#define sumV(a) accumulate(a.begin(), a.end(), 0LL)
+#define mxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = LLONG_MAX;
+const ld EPS = LLONG_MIN;
+
+using namespace std;
+
+void solve() {
+    ll n, m; cin >> n >> m;
+    string s; cin >> s;
+    map<char, ll> mp;
+    for(auto i : s) {
+        mp[i]++;
+    }
+    string check = "ABCDEFG";
+    ll ans = 0;
+    for(auto i : check) {
+        if(mp[i] < m) ans += (m - mp[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    opt();
+    tc() {
+        solve();
+    }
+    return 0;
+}
