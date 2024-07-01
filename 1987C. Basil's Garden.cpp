3f444e1eee28365d9e14543d3a536f596eaa123c
Sudipto Dey Himel <136897150+sudipto-dey-himel@users.noreply.github.com>
2024-07-01 12:35:53 +0600

    Create 1987C. Basil's Garden.cpp

---
+++ 1987C. Basil's Garden.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+
+#define endl '\n'
+#define fi first
+#define se second 
+#define pb push_back
+#define ppb pop_back
+#define HA cout<<"YES"<<endl
+#define NA cout<<"NO"<<endl
+#define mem(a, b) memset(a, b, sizeof(a))
+#define tc() ll t;cin>>t;while(t--)
+#define all(V) (V).begin(), (V).end()
+#define bug(a) cout << #a << " : " << a << endl;
+#define sumV(a) accumulate(a.begin(), a.end(), 0LL)
+#define mxV(V) *max_element(V.begin(), V.end())
+#define mnV(V) *min_element(V.begin(), V.end())
+#define mx(a, b, c) max(a, max(b, c))
+#define mn(a, b, c) min(a, min(b, c))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define opt() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+const int MAX_SIZE = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = LLONG_MAX;
+const ld EPS = LLONG_MIN;
+
+void solve() {
+    ll n; cin >> n;
+    vector<ll> V(n);
+    for(auto &x : V) cin >> x;
+
+    ll ans = 0;
+    for(ll i=n-1; i>=0; i--) {
+        (V[i] > ans) ? ans = V[i] : ans++; 
+    }
+
+    cout << ans << endl;
+}
+
+// void solve() {              //   also accepted
+//     ll n; cin >> n;
+//     vector<ll> V(n);
+//     for(auto &x : V) cin >> x;
+
+//     ll maxi = 0;
+//     for(ll i=0; i<n; i++) {
+//         maxi = max(maxi, V[i] + i);
+//     }
+//     cout << maxi << endl;
+// }
+
+int main() {
+    opt();
+    tc() {
+        solve();
+    }
+    return 0;
+}
